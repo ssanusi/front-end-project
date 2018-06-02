bd069c5dc046b53f7fcf1d6cdde15bd91e9c61e8
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-06-02 09:35:02 +0100

    created my Component file and CSS file

---
+++ lambda-note/src/components/SideNav/SideNav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import  './index.css';
+
+class SideNav extends Component {
+    render() {
+        return (
+            <div>
+                THIS IS SIDE NAV
+            </div>
+        );
+    }
+}
+
+export default SideNav;
