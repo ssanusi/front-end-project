f38f281579e51dbc022e8862458fc7577e07fb0c
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-06-02 20:23:41 +0100

    added React-Router, Redux to the project

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const dummyReducer = ( state = [], action ) => state;
+
+const store = createStore(dummyReducer);
+
+ReactDOM.render(
+    (
+    <Provider store ={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+    ),
+    document.getElementById('root'));
 registerServiceWorker();
