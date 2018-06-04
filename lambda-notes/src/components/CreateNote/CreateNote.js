76b160d9a535318870d9c8c2e3ca9cc1d93fedbf
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-06-04 09:26:00 +0100

    createNote styling and component structure completed

--- lambda-notes/src/components/CreateNote/CreateNote.js
+++ lambda-notes/src/components/CreateNote/CreateNote.js
@@ -4,8 +4,17 @@ import  './index.css';
 class CreateNote extends Component {
     render() {
         return (
-            <div>
-                CREATE NOTE COMPONENT
+            <div className="CreateNote_container">
+                    <div className="note_Header">
+                        <h3>
+                            Create New Note:
+                        </h3>
+                </div>
+                <div className="form_container">
+                    <input type="text" placeholder ="Note Title"/>
+                    <textarea rows="30" placeholder ="Note Content"></textarea>
+                    <div className="submit">SAVE</div>
+                </div>
             </div>
         );
     }
--- lambda-notes/src/components/MainContent/MainContent.js
+++ lambda-notes/src/components/MainContent/MainContent.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import  './index.css';
 import NoteList from '../NoteList/NoteList';
 import NoteView from '../NoteView/NoteView';
+import CreateNote from '../CreateNote/CreateNote';
+
 
 
 class MainContent extends Component {
     render() {
         return (
             <div  className = "main-container">
-               <NoteView />
+               <CreateNote />
             </div>
         );
     }
